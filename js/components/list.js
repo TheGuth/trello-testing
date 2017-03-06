296f26b40cc78188aa5e55cf27f5317a9b387cee
Laura Wolfart <laurawolfart@gmail.com>
2017-03-06 12:44:51 -0500

    added card, list and board components

---
+++ js/components/list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './card';
+
+export default function List() {
+	return (
+		<div>
+			<Card />
+			<Card />
+			<Card />
+		</div>	
+	);
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './components/board';
+
 require('babel-polyfill');
 
+document.addEventListener('DOMContentLoaded', () => 
+    ReactDOM.render(<Board />, document.getElementById('app'))
+);
